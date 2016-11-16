5d5ed7d3bc96a7864af68fd0506979d7c35f1a30
SnowGushiGit <snow.akogi.pgel@gmail.com>
2016-11-16 09:16:06 +0900

    update gitignore

---
+++ dynet_msr/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+
+sys.path.append('.')
+
+
+class:
+    """
+    """
+
+    def __init__(self):
+
+        """
+    """
+
+    def(self):
+
+        """
+    """
